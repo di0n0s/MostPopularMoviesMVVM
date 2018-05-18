bced5e6f273e1e320f12e6d05d9c185d22a60f9c
di0n0s <sfuentescarrique@gmail.com>
2018-05-18 22:40:57 +0200

    Interceptor added to Retrofit

--- presentation/src/main/java/com/example/sfcar/mostpopularmovies/injector/modules/MoviesApiModule.kt
+++ presentation/src/main/java/com/example/sfcar/mostpopularmovies/injector/modules/MoviesApiModule.kt
@@ -2,6 +2,7 @@ package com.example.sfcar.mostpopularmovies.injector.modules
 
 import com.example.sfcar.mostpopularmovies.data.net.ApiConstants
 import com.example.sfcar.mostpopularmovies.data.net.MoviesApiService
+import com.example.sfcar.mostpopularmovies.data.net.interceptor.AuthInterceptor
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
@@ -15,8 +16,9 @@ class MoviesApiModule {
 
     @Provides
     @Singleton
-    fun provideMoviesApi(): MoviesApiService {
+    fun provideMoviesApi(authInterceptor: AuthInterceptor): MoviesApiService {
         val httpClient = OkHttpClient.Builder()
+                .addInterceptor(authInterceptor)
                 .connectTimeout(ApiConstants.TIMEOUT_CONNECTION_VALUE, TimeUnit.SECONDS)
                 .readTimeout(ApiConstants.TIMEOUT_READ_VALUE, TimeUnit.SECONDS)
                 .writeTimeout(ApiConstants.TIMEOUT_WRITE_VALUE, TimeUnit.SECONDS)
