8f14aa5017b25c55652965e585db5ad0839ca59c
di0n0s <sfuentescarrique@gmail.com>
2018-05-18 21:42:10 +0200

    MoviesApiModule created and added to ApplicationComponent

--- presentation/src/main/java/com/example/sfcar/mostpopularmovies/injector/components/ApplicationComponent.kt
+++ presentation/src/main/java/com/example/sfcar/mostpopularmovies/injector/components/ApplicationComponent.kt
@@ -1,12 +1,13 @@
 package com.example.sfcar.mostpopularmovies.injector.components
 
 import com.example.sfcar.mostpopularmovies.injector.modules.ApplicationModule
+import com.example.sfcar.mostpopularmovies.injector.modules.MoviesApiModule
 import com.example.sfcar.mostpopularmovies.views.base.BaseActivity
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [ApplicationModule::class])
+@Component(modules = [ApplicationModule::class, MoviesApiModule::class])
 interface ApplicationComponent {
 
     fun inject(baseActivity: BaseActivity)
---
+++ presentation/src/main/java/com/example/sfcar/mostpopularmovies/injector/modules/MoviesApiModule.kt
@@ -0,0 +1,28 @@
+package com.example.sfcar.mostpopularmovies.injector.modules
+
+import com.example.sfcar.mostpopularmovies.data.net.ApiConstants
+import com.example.sfcar.mostpopularmovies.data.net.MoviesApiService
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+class MoviesApiModule {
+
+    @Provides
+    @Singleton
+    fun provideMoviesApi(): MoviesApiService {
+        val httpClient = OkHttpClient.Builder()
+                .connectTimeout(ApiConstants.TIMEOUT_CONNECTION_VALUE, TimeUnit.SECONDS)
+                .readTimeout(ApiConstants.TIMEOUT_READ_VALUE, TimeUnit.SECONDS)
+                .writeTimeout(ApiConstants.TIMEOUT_WRITE_VALUE, TimeUnit.SECONDS)
+        val builder = Retrofit.Builder()
+                .baseUrl(ApiConstants.ENDPOINT)
+                .addConverterFactory(GsonConverterFactory.create())
+        return builder.client(httpClient.build()).build().create(MoviesApiService::class.java)
+    }
+}
