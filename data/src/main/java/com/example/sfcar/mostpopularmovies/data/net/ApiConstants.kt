1099e51199995ed7294722da846319fd5a4bbec7
di0n0s <sfuentescarrique@gmail.com>
2018-05-18 22:40:00 +0200

    AuthInterceptor added

--- data/src/main/java/com/example/sfcar/mostpopularmovies/data/net/ApiConstants.kt
+++ data/src/main/java/com/example/sfcar/mostpopularmovies/data/net/ApiConstants.kt
@@ -7,4 +7,8 @@ object ApiConstants {
     const val TIMEOUT_CONNECTION_VALUE = 60L
     const val TIMEOUT_READ_VALUE = 60L
     const val TIMEOUT_WRITE_VALUE = 60L
+    const val API_KEY = "93aea0c77bc168d8bbce3918cefefa45"
+    const val QUERY_PARAM_LANGUAGE = "language"
+    const val QUERY_PARAM_API_KEY = "api_key"
+    const val QUERY_PARAM_REGION = "region"
 }
---
+++ data/src/main/java/com/example/sfcar/mostpopularmovies/data/net/interceptor/AuthInterceptor.kt
@@ -0,0 +1,29 @@
+package com.example.sfcar.mostpopularmovies.data.net.interceptor
+
+import com.example.sfcar.mostpopularmovies.data.net.ApiConstants
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.util.*
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AuthInterceptor @Inject constructor() : Interceptor{
+
+    override fun intercept(chain: Interceptor.Chain?): Response {
+        var request = chain?.request()
+        var url = request?.url()
+
+        val urlBuilder = url?.newBuilder()
+        urlBuilder?.addEncodedQueryParameter(ApiConstants.QUERY_PARAM_API_KEY, ApiConstants.API_KEY)
+        urlBuilder?.addEncodedQueryParameter(ApiConstants.QUERY_PARAM_LANGUAGE, Locale.getDefault().displayLanguage)
+        urlBuilder?.addEncodedQueryParameter(ApiConstants.QUERY_PARAM_REGION, Locale.getDefault().toString())
+
+        url = urlBuilder?.build()
+        request = request?.newBuilder()?.url(url)?.build()
+        println("URl -> " + request?.url())
+        return chain?.proceed(request)!!
+    }
+
+
+}
