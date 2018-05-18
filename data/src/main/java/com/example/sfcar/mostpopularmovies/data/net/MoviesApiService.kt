dc5e8fffba99b1ad6069aa8d6da0f9afdc7014e4
di0n0s <sfuentescarrique@gmail.com>
2018-05-18 23:45:12 +0200

    getPopularMovies added to MoviesApiService

--- data/src/main/java/com/example/sfcar/mostpopularmovies/data/net/MoviesApiService.kt
+++ data/src/main/java/com/example/sfcar/mostpopularmovies/data/net/MoviesApiService.kt
@@ -1,4 +1,13 @@
 package com.example.sfcar.mostpopularmovies.data.net
 
+import com.example.sfcar.mostpopularmovies.data.entities.base.ListBaseResponseDto
+import com.example.sfcar.mostpopularmovies.data.entities.base.MovieResponseDto
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface MoviesApiService {
+
+    @GET("movie/popular")
+    fun getPopularMovies(@Query("page") page: Int): Observable<ListBaseResponseDto<MovieResponseDto>>
 }
