bbe82e575000d58215ab1a5b70375eba0a0f889e
di0n0s <sfuentescarrique@gmail.com>
2018-05-18 23:58:16 +0200

    MoviesDataSource renamed to MoviesDataStore

--- data/src/main/java/com/example/sfcar/mostpopularmovies/data/repository/datasource/MoviesDataStore.kt
+++ data/src/main/java/com/example/sfcar/mostpopularmovies/data/repository/datasource/MoviesDataStore.kt
@@ -3,7 +3,7 @@ package com.example.sfcar.mostpopularmovies.data.repository.datasource
 import com.example.sfcar.mostpopularmovies.data.entities.base.MovieListResponseDto
 import io.reactivex.Observable
 
-interface MoviesDataSource {
+interface MoviesDataStore {
 
     fun getPopularMovies(page: Int): Observable<MovieListResponseDto>
 }
