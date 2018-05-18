56442ff5bdc9bf70e7bcf7c990561c46f1b0806a
di0n0s <sfuentescarrique@gmail.com>
2018-05-18 21:40:58 +0200

    MoviesApiService and ApiConstants added

---
+++ data/src/main/java/com/example/sfcar/mostpopularmovies/data/net/ApiConstants.kt
@@ -0,0 +1,10 @@
+package com.example.sfcar.mostpopularmovies.data.net
+
+object ApiConstants {
+
+    const val BASE_URL = "http://api.themoviedb.org"
+    const val ENDPOINT = "$BASE_URL/3/"
+    const val TIMEOUT_CONNECTION_VALUE = 60L
+    const val TIMEOUT_READ_VALUE = 60L
+    const val TIMEOUT_WRITE_VALUE = 60L
+}
---
+++ data/src/main/java/com/example/sfcar/mostpopularmovies/data/net/MoviesApiService.kt
@@ -0,0 +1,4 @@
+package com.example.sfcar.mostpopularmovies.data.net
+
+interface MoviesApiService {
+}
