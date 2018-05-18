42f5cda7f001155e9858981b356842d2fb83fc60
di0n0s <sfuentescarrique@gmail.com>
2018-05-18 20:06:14 +0200

    BaseActivity added

---
+++ presentation/src/main/java/com/example/sfcar/mostpopularmovies/views/base/BaseActivity.kt
@@ -0,0 +1,44 @@
+package com.example.sfcar.mostpopularmovies.views.base
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import com.example.sfcar.mostpopularmovies.R
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    protected var currentTag: String? = null
+    protected var currentFragment: Fragment? = null
+
+    companion object {
+        private const val CURRENT_FRAGMENT_TAG: String = "CURRENT_FRAGMENT_TAG"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+        initializeFragmentAndTAG(savedInstanceState)
+        beginTransaction()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        supportFragmentManager.putFragment(outState, currentTag, currentFragment)
+        outState?.putString(CURRENT_FRAGMENT_TAG, currentTag)
+        super.onSaveInstanceState(outState)
+    }
+
+    private fun initializeFragmentAndTAG(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null || !savedInstanceState.containsKey(currentTag)) {
+            createFragmentAndSettingTAG()
+        } else {
+            currentTag = savedInstanceState.getString(CURRENT_FRAGMENT_TAG)
+            currentFragment = supportFragmentManager.getFragment(savedInstanceState, currentTag)
+        }
+    }
+
+    private fun beginTransaction() =
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, currentFragment, currentTag).commit()
+
+    abstract fun createFragmentAndSettingTAG()
+
+}
