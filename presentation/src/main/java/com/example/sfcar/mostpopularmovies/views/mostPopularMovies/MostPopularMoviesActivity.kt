bf0d84bc638cd525e9880ee4be67fad72398d7d3
di0n0s <sfuentescarrique@gmail.com>
2018-05-19 22:39:10 +0200

    onCreate removed and layout behaviour added to layout

--- presentation/src/main/java/com/example/sfcar/mostpopularmovies/views/mostPopularMovies/MostPopularMoviesActivity.kt
+++ presentation/src/main/java/com/example/sfcar/mostpopularmovies/views/mostPopularMovies/MostPopularMoviesActivity.kt
@@ -1,15 +1,8 @@
 package com.example.sfcar.mostpopularmovies.views.mostPopularMovies
 
-import android.os.Bundle
 import com.example.sfcar.mostpopularmovies.views.base.BaseActivity
-import kotlinx.android.synthetic.main.activity_base.*
 
 class MostPopularMoviesActivity : BaseActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setSupportActionBar(toolbar)
-    }
 
     override fun createFragmentAndSettingTAG() {
         currentFragment = MostPopularMoviesFragment.newInstance()
