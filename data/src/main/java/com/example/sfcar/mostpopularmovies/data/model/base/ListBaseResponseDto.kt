8e150b54a2154f11ba9b275c3a0995d1ea609cac
di0n0s <sfuentescarrique@gmail.com>
2018-05-19 22:12:28 +0200

    current page has to be at least one

--- data/src/main/java/com/example/sfcar/mostpopularmovies/data/model/base/ListBaseResponseDto.kt
+++ data/src/main/java/com/example/sfcar/mostpopularmovies/data/model/base/ListBaseResponseDto.kt
@@ -1,6 +1,6 @@
 package com.example.sfcar.mostpopularmovies.data.model.base
 
-open class ListBaseResponseDto<T>(val page: Int = 0,
+open class ListBaseResponseDto<T>(val page: Int = 1,
                                   val results: List<T>? = null,
                                   val total_results: Int = 0,
                                   val total_pages: Int = 0)
--- presentation/src/main/java/com/example/sfcar/mostpopularmovies/model/MovieListPaginationViewModel.kt
+++ presentation/src/main/java/com/example/sfcar/mostpopularmovies/model/MovieListPaginationViewModel.kt
@@ -1,5 +1,5 @@
 package com.example.sfcar.mostpopularmovies.model
 
 data class MovieListPaginationViewModel(var pagesNumber: Int = 0,
-                                        var currentPage: Int = 0,
+                                        var currentPage: Int = 1,
                                         var movieList: ArrayList<MovieViewModel> = ArrayList())
