8385e2269213aac76a9a0f361c9b71dbae69c94b
di0n0s <sfuentescarrique@gmail.com>
2018-05-19 22:25:58 +0200

    MoviePopularMoviesView provided by Dagger graph

--- presentation/src/main/java/com/example/sfcar/mostpopularmovies/injector/modules/MostPopularMoviesModule.kt
+++ presentation/src/main/java/com/example/sfcar/mostpopularmovies/injector/modules/MostPopularMoviesModule.kt
@@ -1,6 +1,17 @@
 package com.example.sfcar.mostpopularmovies.injector.modules
 
+import com.example.sfcar.mostpopularmovies.injector.PerFragment
+import com.example.sfcar.mostpopularmovies.views.mostPopularMovies.MostPopularMoviesFragment
+import com.example.sfcar.mostpopularmovies.views.mostPopularMovies.MostPopularMoviesView
 import dagger.Module
+import dagger.Provides
 
 @Module
-class MostPopularMoviesModule
+class MostPopularMoviesModule(private val mostPopularMoviesFragment: MostPopularMoviesFragment) {
+
+    @Provides
+    @PerFragment
+    fun provideMostPopularMoviesView(): MostPopularMoviesView {
+        return mostPopularMoviesFragment
+    }
+}
