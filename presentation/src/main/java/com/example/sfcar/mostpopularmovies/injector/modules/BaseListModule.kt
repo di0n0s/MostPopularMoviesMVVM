db81bd002eb4a9a483faf7f9d31435ba77e81d29
di0n0s <sfuentescarrique@gmail.com>
2018-05-20 17:03:55 +0200

    provide LinearLayoutManager added to di

--- presentation/src/main/java/com/example/sfcar/mostpopularmovies/injector/modules/BaseListModule.kt
+++ presentation/src/main/java/com/example/sfcar/mostpopularmovies/injector/modules/BaseListModule.kt
@@ -1,15 +1,23 @@
 package com.example.sfcar.mostpopularmovies.injector.modules
 
+import android.content.Context
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.StaggeredGridLayoutManager
 import com.example.sfcar.mostpopularmovies.injector.PerFragment
 import dagger.Module
 import dagger.Provides
 
 @Module
-class BaseListModule {
+class BaseListModule(private val context: Context) {
 
     @Provides
     @PerFragment
     fun provideStaggeredGridLayoutManager(): StaggeredGridLayoutManager = StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
+
+    @Provides
+    @PerFragment
+    fun provideLinearLayoutManager(context: Context): LinearLayoutManager {
+        return LinearLayoutManager(context)
+    }
 
 }
