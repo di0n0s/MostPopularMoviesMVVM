9f9c9ae1968b7db2d0fc8b6ad76cd63054220eb9
di0n0s <sfuentescarrique@gmail.com>
2018-05-20 17:02:30 +0200

    Image url constants added

--- data/src/main/java/com/example/sfcar/mostpopularmovies/data/net/ApiConstants.kt
+++ data/src/main/java/com/example/sfcar/mostpopularmovies/data/net/ApiConstants.kt
@@ -3,6 +3,8 @@ package com.example.sfcar.mostpopularmovies.data.net
 object ApiConstants {
 
     const val BASE_URL = "http://api.themoviedb.org"
+    const val BASE_URL_IMAGE = "https://image.tmdb.org/t/p"
+    const val IMAGE_SIZE_W342 = "/w342"
     const val ENDPOINT = "$BASE_URL/3/"
     const val TIMEOUT_CONNECTION_VALUE = 60L
     const val TIMEOUT_READ_VALUE = 60L
